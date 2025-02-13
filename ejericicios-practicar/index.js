08e2b46259f971678c493d06a9640359fc458ae8
Joan-02 <jsalvadorsb@gmail.com>
2025-02-13 15:26:37 +0100

    ejericico personal mas desarrollado

--- ejericicios-practicar/index.js
+++ ejericicios-practicar/index.js
@@ -11,12 +11,34 @@ formulario.addEventListener('submit', function(event) {
     } else {
         document.querySelector("#error-message").innerText = '';
         const task = document.createElement('li');
+        const eliminateButton = document.createElement('button');
+        eliminateButton.innerText = "Eliminar";
+        const editButton = document.createElement('button');
+        editButton.innerText = "Edit";
+
+
         task.innerHTML = `${taskText}`;
 
         const tasksList = document.querySelector('#task-list');
         tasksList.appendChild(task);
+
+        task.addEventListener('click', () => {
+            task.classList.toggle('completed');
+        })
+
+        task.appendChild(eliminateButton); 
+        task.appendChild(editButton);
+
+        eliminateButton.addEventListener('click', () => {
+            task.remove();
+        });
+
+        editButton.addEventListener('click', () => {
+            
+        });
+
         input.value = "";
     }
-    
 });
+
 
