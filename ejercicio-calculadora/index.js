6844147d8992e19c1201bc2166cc128a79c81ab8
Joan-02 <jsalvadorsb@gmail.com>
2025-02-22 18:25:55 +0100

    ejercicio calculadora terminado

---
+++ ejercicio-calculadora/index.js
@@ -0,0 +1,40 @@
+const numberButtons = document.querySelectorAll('.number-button');  // Selecciona todos los botones
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        console.log('click en un número');
+        addNumberToResult(button);  // Pasamos el botón actual
+    });
+});
+
+const operators = document.querySelectorAll('.operator');  // Selecciona todos los operadores
+
+operators.forEach(operator => {
+    operator.addEventListener('click', function () {
+        console.log('click en un operator');
+        addOperatorToResult(operator);  // Pasamos el operator actual
+    });
+});
+
+const result = document.querySelector('#result');
+
+function addNumberToResult(button) {
+    result.textContent += button.textContent;  // Agrega el contenido del botón presionado al resultado
+}
+
+function addOperatorToResult(operator) {
+    result.textContent += operator.textContent;  // Agrega el contenido del botón presionado al resultado
+}
+
+document.querySelector('#equals').addEventListener('click', function () {
+    console.log('igual')
+    operationNumbers();
+})
+
+function operationNumbers () {
+    let expression = result.textContent;
+    console.log(expression);
+    let operationResult = eval(expression);
+    console.log(operationResult);
+    result.textContent = operationResult;
+}
