3c85b6a394c1ef0b9b1912fdd36b01fe3e31d8a2
Joan Sanchis <jsalvadorsb@gmail.com>
2025-03-12 15:22:36 +0100

    JS del ejercicio buscador de pelicula

--- ejercicio-buscador-de-peliculas/index.js
+++ ejercicio-buscador-de-peliculas/index.js
@@ -1,20 +1,64 @@
-const tasksList = document.querySelector('#task-list');
-tasksList.appendChild(task);
+const peliculas = [
+    {
+      titulo: "El Padrino",
+      año: 1972,
+    },
+    {
+      titulo: "Titanic",
+      año: 1997,
+    },
+    {
+      titulo: "El Señor de los Anillos: La Comunidad del Anillo",
+      año: 2001,
+    },
+    {
+      titulo: "Interestelar",
+      año: 2014,
+    },
+    {
+      titulo: "Matrix",
+      año: 1999,
+    },
+    {
+      titulo: "El Rey León",
+      año: 1994,
+    },
+    {
+      titulo: "Forrest Gump",
+      año: 1994,
+    },
+    {
+      titulo: "El Caballero Oscuro",
+      año: 2008,
+    }
+];
 
-task.addEventListener('click', () => {
-    task.classList.toggle('completed');
-})
+function mostrarPeliculas(peliculas) {
+    const listaPeliculas = document.querySelector('#resultados');
+    listaPeliculas.innerHTML = '';
 
-task.appendChild(eliminateButton); 
+    peliculas.forEach((pelicula) => {
+        const li = document.createElement('li');
+        li.className = 'tituloPelicula';
+        li.innerText = `${pelicula.titulo} ${pelicula.año}`;
+        listaPeliculas.appendChild(li);
+    })
+};
 
-eliminateButton.addEventListener('click', () => {
-    task.remove();
-});
+function filtrarPeliculas() {
+    const inputBuscador = document.querySelector("#buscarPelicula");
+    const textoBuscado = inputBuscador.value.toLowerCase();
 
-const arrayTasks = getTasks();
-arrayTasks.push(taskText);
-localStorage.setItem('task', JSON.stringify(arrayTasks));
+    console.log("Texto buscado: ", textoBuscado); // Muestra lo que está buscando el usuario
 
-input.value = "";
-}
-});
+    const peliculasFiltradas = peliculas.filter((pelicula) => {
+        const tituloPelicula = pelicula.titulo.toLowerCase();
+        console.log("Comparando: ", tituloPelicula, " con ", textoBuscado); // Muestra cada título comparado con el texto buscado
+
+        return tituloPelicula.includes(textoBuscado); // Compara si el título contiene el texto buscado
+    });
+
+    console.log("Películas encontradas: ", peliculasFiltradas); // Muestra las películas que coinciden
+};
+
+mostrarPeliculas(peliculas);
